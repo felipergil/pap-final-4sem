5b1381cd181f3ccb4f6236ec5ceea925c471b2c7
Felipe Rodrigues <NOTFELIPE@192.168.2.120>
2016-06-30 15:08:44 -0300

    Adicionado Web Service do Correios para calculo do frete.

--- src/java/Bean/CarrinhoBean.java
+++ src/java/Bean/CarrinhoBean.java
@@ -9,11 +9,16 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import Model.Produto;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
+import javax.xml.ws.WebServiceRef;
+import org.tempuri.CResultado;
+import org.tempuri.CServico;
+import org.tempuri.CalcPrecoPrazoWS;
 
 /**
  *
@@ -23,12 +28,37 @@ import javax.faces.context.FacesContext;
 @Named(value = "carrinhoBean")
 @SessionScoped
 public class CarrinhoBean implements Serializable {
+
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/ws.correios.com.br/calculador/CalcPrecoPrazo.asmx.wsdl")
+    private CalcPrecoPrazoWS service;
     
     @EJB
     PedidoService pedidoService;
 
     List<Produto> produtos = new ArrayList<>();
-    public String email;
+    private String email;
+    private String cepDestino;
+    private Double valorFrete;
+
+    public Double getValorFrete() {
+        return valorFrete;
+    }
+
+    public void setValorFrete(Double valorFrete) {
+        this.valorFrete = valorFrete;
+    }
+    
+    
+
+    public String getCepDestino() {
+        return cepDestino;
+    }
+
+    public void setCepDestino(String cepDestino) {
+        this.cepDestino = cepDestino;
+    }
+    
+    
 
     public String getEmail() {
         return email;
@@ -119,12 +149,12 @@ public class CarrinhoBean implements Serializable {
         for (Map.Entry<Produto, Integer> entry : map.entrySet()) {
             Pedido pedido = new Pedido();
             pedido.setDatahora("21/06/16");
-            pedido.setFrete(1.1);
+            pedido.setFrete(calcularFrete());
             pedido.setProduto(entry.getKey().getTitulo());
             pedido.setQuantidade(entry.getValue());
             pedido.setStatus("Aguardando Pagamento");
             pedido.setValortotal(entry.getKey().getValor() * entry.getValue());
-            pedido.setValorcompra(somarValorTotal());
+            pedido.setValorcompra(somarValorTotal() + calcularFrete());
             pedido.setIdentificador(identificador);
             pedido.setEmail(getEmail());
             pedidoService.addPedido(pedido);
@@ -132,7 +162,21 @@ public class CarrinhoBean implements Serializable {
         produtos.removeAll(produtos);
         
     }
+
+    private CResultado calcPreco(java.lang.String nCdEmpresa, java.lang.String sDsSenha, java.lang.String nCdServico, java.lang.String sCepOrigem, java.lang.String sCepDestino, java.lang.String nVlPeso, int nCdFormato, java.math.BigDecimal nVlComprimento, java.math.BigDecimal nVlAltura, java.math.BigDecimal nVlLargura, java.math.BigDecimal nVlDiametro, java.lang.String sCdMaoPropria, java.math.BigDecimal nVlValorDeclarado, java.lang.String sCdAvisoRecebimento) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        org.tempuri.CalcPrecoPrazoWSSoap port = service.getCalcPrecoPrazoWSSoap();
+        return port.calcPreco(nCdEmpresa, sDsSenha, nCdServico, sCepOrigem, sCepDestino, nVlPeso, nCdFormato, nVlComprimento, nVlAltura, nVlLargura, nVlDiametro, sCdMaoPropria, nVlValorDeclarado, sCdAvisoRecebimento);
+    }
     
-    
+    public Double calcularFrete() {
+        CResultado res = calcPreco("", "", "40010", "09641000", getCepDestino(), "0.5", 1, new BigDecimal(16), new BigDecimal(5), new BigDecimal(11), new BigDecimal(10), "N", new BigDecimal(0), "N");
+        List<CServico> list = res.getServicos().getCServico();
+        for(CServico c : list) {
+            this.valorFrete = Double.parseDouble(c.getValor().replace(",", "."));
+        }
+        return valorFrete;
+    }
    
 }
